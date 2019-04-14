d10acf6d84b18ed2ad0afbf6a68d67d395dc1d9a
Jakub Jirutka <jakub@jirutka.cz>
2019-04-14 23:33:56 +0200

    Update asciidoctor.js to 2.0.0

--- lib/asciidoctor/interdoc_reftext/resolver.rb
+++ lib/asciidoctor/interdoc_reftext/resolver.rb
@@ -20,17 +20,8 @@ module Asciidoctor::InterdocReftext
 
       logger ||= if defined? ::Asciidoctor::LoggerManager
         ::Asciidoctor::LoggerManager.logger
-      elsif defined? ::Logger
-        ::Logger.new(STDERR)
       else
-        # Fake logger for Opal.
-        # TODO: Remove after update to Asciidoctor 1.5.7 or Opal with Logger.
-        Object.new.tap do |o|
-          # rubocop: disable MethodMissing
-          def o.method_missing(_, *args)
-            STDERR.puts(*args)
-          end
-        end
+        ::Logger.new(STDERR)
       end
 
       @document = document
